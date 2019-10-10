aea6c7c61491a4837f92f9345854403ee5e89a67
ZhangWeiC <scty8888@163.com>
2019-10-10 11:41:35 +0800

    A: 对歌手数据进行二次处理

--- vue-app/src/views/singer/singer.vue
+++ vue-app/src/views/singer/singer.vue
@@ -6,6 +6,11 @@
 <script type="text/ecmascript-6">
   import {getSingerList} from 'api/singer'
   import {ERR_OK} from 'api/config'
+
+  import Singer from 'common/js/singer'
+
+  const HOT_TITLE = 'hot'
+  const HOT_LENGTH = 10  
   
   export default {
     data() {
@@ -20,11 +25,50 @@
       _getSingerList() {
         getSingerList().then((res) => {
           if (res.code === ERR_OK) {
-            console.log(res.data.list);
+            console.log( this._normalizeSinger(res.data.list) )
           }
         }).catch(err => {
           console.log(err)
         })
+      },
+      _normalizeSinger(list) {
+        let map = {
+          hot: {
+            title: HOT_TITLE,
+            items: []
+          }
+        }
+        list.forEach( (item, index) => {
+          if (index < HOT_LENGTH) {
+            map.hot.items.push(new Singer(item.Fsinger_mid, item.Fsinger_name))
+          }
+
+          let idx = item.Findex
+          if (!map[idx]) {
+            map[idx] = {
+              title: idx,
+              item: []
+            }
+          }
+          map[idx].item.push(new Singer(item.Fsinger_mid, item.Fsinger_name))  
+        })
+
+        let hot = []
+        let ret = []
+        for(var key in map){
+          let val = map[key]
+
+          if (val.title === 'hot') {
+            hot.push(val)
+          }else if(val.title.match(/[a-zA-Z]/)){
+            ret.push(val)
+          }
+        }
+
+        ret.sort((a, b) => {
+          return a.title.charCodeAt(0)-b.title.charCodeAt(0)
+        })
+        return hot.concat(ret)
       }
     }
   }
